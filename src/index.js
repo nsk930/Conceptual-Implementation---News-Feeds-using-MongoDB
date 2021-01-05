32eb9e3e21b7e850591ac2c440cdefeaf325b851
nsk930 <nsk930@gmail.com>
2021-01-05 18:45:59 +0530

    mm

--- src/index.js
+++ src/index.js
@@ -8,6 +8,31 @@ const onePageArticleCount = 10
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const express = require('express')
+const app = express()
+const port = 8080
+const {newsArticleModel} = require('./connector')
+const onePageArticleCount = 10
+
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/newFeeds',async(req,res) => {
+    res.send(await newsArticleModel.find().skip(sanitize(req.query.offset, 0)).limit(sanitize(req.query.limit , 10)));
+});
+
+const sanitize = (value, defaultValue) => {
+    if(value === null || value === undefined || isNaN(Number(value))) {
+        return defaultValue;
+    }
+    return Number(value);
+}
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;
 
 
 
