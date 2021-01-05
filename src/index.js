7986b7f9505f0f4da73506feb192cb5a9b0c796f
nsk930 <nsk930@gmail.com>
2021-01-05 19:11:24 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 8080
-
-const onePageArticleCount = 10
-
-
-// Parse JSON bodies (as sent by API clients)
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-const express = require('express')
-const app = express()
-const port = 8080
 const {newsArticleModel} = require('./connector')
 const onePageArticleCount = 10
 
@@ -29,12 +19,6 @@ const sanitize = (value, defaultValue) => {
     }
     return Number(value);
 }
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app;
-
-
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
